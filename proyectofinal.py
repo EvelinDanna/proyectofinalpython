794618aecd11304aa9d1dc3bde401423786d4188
EvelinDanna <salinasfloresevelincorayma@gmail.com>
2026-04-29 15:37:09 -0600

    Add files via upload
    Pasos para crear mi proyecto

---
+++ proyectofinal.py
@@ -0,0 +1,8 @@
+***Creacion de mi carpeta del proyecto ***
+
+Sigue los siguientes pasos para crear el proyecto final, no importando en que computadora se encuentre
+
+1. Crea la carpeta ProyectofinalEvelinyDanna (sustituye nombre por el tuyo), en Documentos va estar carpetas.
+2. Descarga de tu repositorio el proyectofinal.py y las imagenes creadas 
+3. Realioza los cambios y las actividades de cadaleccion.
+4. Sube a su repositorio el proyectofinal.py con los cambios realizados.
