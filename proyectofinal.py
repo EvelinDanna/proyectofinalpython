92902a852c0c328955164f193e81a0ea971697fa
EvelinDanna <salinasfloresevelincorayma@gmail.com>
2026-04-29 15:37:47 -0600

    proyectofinal.py

--- proyectofinal.py
+++ proyectofinal.py
@@ -4,5 +4,5 @@ Sigue los siguientes pasos para crear el proyecto final, no importando en que co
 
 1. Crea la carpeta ProyectofinalEvelinyDanna (sustituye nombre por el tuyo), en Documentos va estar carpetas.
 2. Descarga de tu repositorio el proyectofinal.py y las imagenes creadas 
-3. Realioza los cambios y las actividades de cadaleccion.
+3. Realiza los cambios y las actividades de cadaleccion.
 4. Sube a su repositorio el proyectofinal.py con los cambios realizados.
